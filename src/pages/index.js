a2ea02235150cd29b7756b8a8c72e16829182cdf
ArielElp <ariel@starkware.co>
2022-03-01 11:33:07 +0200

    formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import { HomepageFeatures } from '../components/HomepageFeatures/HomepageFeatures';
+import {HomepageFeatures} from '../components/HomepageFeatures/HomepageFeatures';
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+  const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -19,7 +19,7 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
+  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
