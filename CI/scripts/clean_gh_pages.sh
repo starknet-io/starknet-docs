1cf04c3aa0eea0bc8ee001c0310a8b02a43743f8
Idan Shamam <idan@starkware.co>
2024-05-02 18:09:46 +0300

    Added pipeline and script for gh-pages branch cleanup job

---
+++ CI/scripts/clean_gh_pages.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+AUTH_TOKEN=$1
+OWNER=$2
+REPO=$3
+
+
+function list_closed_prs() {
+    curl -sL \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${AUTH_TOKEN}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/${OWNER}/${REPO}/pulls?state=closed&per_page=100" | jq -r '.[].number'
+}
+
+changed="false"
+
+function delete_matching_dirs() {
+    local closed_prs=($(list_closed_prs))
+    for pr_number in "${closed_prs[@]}"
+    do
+        if [ -d "pr-${pr_number}" ]
+        then
+            echo "Deleting directory pr-${pr_number}"
+            rm -rf "pr-${pr_number}"
+            changed="true"
+        fi
+    done
+}
+
+delete_matching_dirs
+
+if [ $changed == "true" ]
+then
+    exit 1
+fi
+
+exit 0
